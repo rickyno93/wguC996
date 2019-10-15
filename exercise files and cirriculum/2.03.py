790689a330c68bccb808fb350a86c956e966a56c
Jesse Ashby <ashbyj@gmail.com>
2019-10-15 02:43:48 -0500

    init commit

---
+++ exercise files and cirriculum/2.03.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# slow version.  dynamic typing causes type-checking on each iteration of the
+# loop.
+def compute_reciprocals(values):
+    output = np.empty(len(values))
+    for i in range(len(values)):
+        output[i] = 1.0 / values[i]
+    return output
+
+values = np.random.randint(1, 100, size=1000000)
+#print(compute_reciprocals(values))
+
+# vectorized operation that uses numpy's compiled layer
+
+print (1.0 / values)
+
+# more vectorized operations between arrays
+
+print (np.arange(5) / np.arange(1, 6))
+
+x = np.arange(9).reshape(3,3)
+print (2**x)
