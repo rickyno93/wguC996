4cdfa5affe1bbae0727d54e161f6ea5a11059110
Jesse Ashby <ashbyj@gmail.com>
2019-10-19 18:39:03 -0500

    comment fixes

--- c996.py
+++ c996.py
@@ -12,7 +12,7 @@ submissionFile.close()
 soup = bs.BeautifulSoup(r.text,features='html.parser')
 uriList = []
 
-# bs4 usage ideas borrowed from:
+# bs4 usage idea borrowed from:
 # https://www.youtube.com/watch?v=aIPqt-OdmS0
 for url in soup.find_all('a'):
     uri = url.get('href')
@@ -22,8 +22,6 @@ for url in soup.find_all('a'):
         if uri.startswith('/'):
             uriList.append('https://www.census.gov%s' % uri)
 
-# remove last character in string borrowed from:
-# https://stackoverflow.com/questions/33940917/
 for i in range(len(uriList)):
     if uriList[i].endswith('/'):
         uriList[i] = uriList[i][:-1]
@@ -32,8 +30,6 @@ for i in range(len(uriList)):
 # https://www.w3schools.com/python/python_howto_remove_duplicates.asp
 uriList = list(dict.fromkeys(uriList))
 
-# write list to single line csv borrowed from:
-# https://stackoverflow.com/questions/6916542/
 with open('uriList.csv','w') as uriListFile:
     wr = csv.writer(uriListFile, quoting=csv.QUOTE_ALL)
     wr.writerow(uriList)
